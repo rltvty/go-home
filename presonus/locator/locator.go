9c6880623eed465b0f8d017cfbfceb9abc6307fb
Eric Pinzur <epinzur@gmail.com>
2020-05-30 23:05:38 -0500

    can now find speakers and the mixer

--- presonus/locator/locator.go
+++ presonus/locator/locator.go
@@ -1,7 +1,9 @@
 package locator
 
 import (
+	"encoding/binary"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"net"
 	"strings"
 
@@ -100,18 +102,100 @@ func Locate() {
 	}
 	defer handle.Close()
 
+	err = handle.SetBPFFilter("ip broadcast")
+	if err != nil {
+		log.InfoError("Error setting pcap filter", err)
+	}
+
 	// Start processing packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	packetCount := 0
 	for packet := range packetSource.Packets() {
 		// Process packet here
+		log.Debug("")
+		log.Debug("")
 		log.Debug("Got Packet", zap.Any("Packet", packet))
 
 		packetCount++
+
+		// Iterate over all layers, printing out each layer type
+		for _, layer := range packet.Layers() {
+			log.Debug("PACKET LAYER:", zap.Any("LayerType", layer.LayerType()))
+		}
+
+		// Get the Ethernet layer from this packet
+		if ethernetLayer := packet.Layer(layers.LayerTypeEthernet); ethernetLayer != nil {
+			// Get actual Ethernet data from this layer
+			ethernet, _ := ethernetLayer.(*layers.Ethernet)
+			log.Debug("MAC", zap.Any("src", ethernet.SrcMAC), zap.Any("dst", ethernet.DstMAC))
+		}
+
+		// Get the IPv4 layer from this packet
+		if ipv4Layer := packet.Layer(layers.LayerTypeIPv4); ipv4Layer != nil {
+			// Get actual IPv4 data from this layer
+			ipv4, _ := ipv4Layer.(*layers.IPv4)
+			log.Debug("IP", zap.Any("src", ipv4.SrcIP), zap.Any("dst", ipv4.DstIP))
+		}
+
+		// Get the UDP layer from this packet
+		if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+			// Get actual UDP data from this layer
+			udp, _ := udpLayer.(*layers.UDP)
+			log.Debug("Ports", zap.Any("src", udp.SrcPort), zap.Any("dst", udp.DstPort))
+		}
+
+		// Get the Application layer from this packet
+		if app := packet.ApplicationLayer(); app != nil {
+			log.Debug("Payload", zap.Any("data", string(app.Payload())))
+			item, _ := DecodeData(app.Payload(), "locator")
+			if item != nil {
+				log.Debug("Found item", zap.Any(item.Kind, item))
+			}
+		}
 
 		// Only capture 100 and then stop
 		if packetCount > 100 {
 			break
 		}
 	}
+}
+
+type Data struct {
+	Source string
+	Mode string
+	Port uint16
+	Model string
+	MacAddress string
+	Kind string
+}
+
+func DecodeData(payload []byte, source string) (*Data, error) {
+	log := logwrapper.GetInstance()
+	if string(payload[0:2]) == "UC" {
+		code := binary.LittleEndian.Uint16(payload[6:])
+		switch code {
+		case 16708:
+			log.Debug("Found Presounus Broadcast")
+			if len(payload) <= 50 {
+				return &Data{
+					Source: source,
+					Mode: "broadcast",
+					Port: binary.LittleEndian.Uint16(payload[4:]),
+					Model: strings.TrimSpace(strings.ReplaceAll(string(payload[16:28]), string(0), " ")),
+					MacAddress: string(payload[28:45]),
+					Kind: "speaker",
+				}, nil
+			} else {
+				return &Data{
+					Source: source,
+					Mode: "broadcast",
+					Port: binary.LittleEndian.Uint16(payload[4:]),
+					Model: strings.TrimSpace(strings.ReplaceAll(string(payload[32:50]), string(0), " ")),
+					MacAddress: "",
+					Kind: "mixer",
+				}, nil
+			}
+		}
+	}
+	return nil, nil
 }
