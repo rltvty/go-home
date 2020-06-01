43ffb94f3067c4a4c4c7fea81829ec7494a72e73
Eric Pinzur <epinzur@gmail.com>
2020-05-31 21:44:35 -0500

    connected to speaker successfully

---
+++ presonus/connection/connection_test.go
@@ -0,0 +1,17 @@
+package connection_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+
+	. "github.com/rltvty/go-home/presonus/connection"
+)
+
+
+//{"speaker": {"Source":"locator","Mode":"broadcast","Port":53781,"Model":"SL328AI SPK","MacAddress":"00:0A:92:D6:66:EE","Kind":"speaker"}}"10.10.10.230"
+
+var _ = Describe("Connection", func() {
+	Describe("Connect", func() {
+		StartManager()
+	})
+})
